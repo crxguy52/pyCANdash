a188ae53dfc37e8e7ccf0d94cc674b1c93492dfd
Victor Zaccardo <victorzaccardo@gmail.com>
2025-03-07 13:26:19 -0500

    Updated tx_can to test clusterCommander

--- src/pyCANdash/examples/tx_can.py
+++ src/pyCANdash/examples/tx_can.py
@@ -3,48 +3,53 @@ import cantools
 import time
 
 
-if __name__ == "__main__":
-    # def log_main(file_name: str, bus_arg: None | list[BusDescriptor], max_chunk_size: bool, suffix_format: str) -> None:
-    """Log a live bus to a disk.
 
-    Args:
-        file_name: name of file to write to
-        bus_arg: name of bus to read from
-        max_chunk_size: max chunk size in bytes of the disk bus to rotate on
-        suffix_format: suffix format of the file/rotated log file
-    """
+dbPath = "/home/victorzaccardo/git/pyCANdash/dbc/gmlan_v1.5.dbc"
 
-    dbPath = "./config_files/gmlan_v1.4.dbc"
-    chanStr = 'virtual'
+print("Loading " + dbPath)
+db = cantools.database.load_file(dbPath)
 
-    print("Loading " + dbPath)
-    db = cantools.database.load_file(dbPath)
+print("Connecting to " + 'can1')
+bus = can.interface.Bus(channel = 'can1', interface = 'socketcan', fd=False)
 
-    print("Connecting to " + chanStr)
-    bus = can.Bus(interface=chanStr)
+
+
+def create_message(db, msgName, sigName, sigVal):
 
     # Create a GMLAN message
     # Start by getting a blank message
-    cantoolsMsg = db.get_message_by_name('engine_general_status_1')
+    cantoolsMsg = db.get_message_by_name(msgName)
 
     # Fill in zero for all of the values
     vals = {}
     for msg in cantoolsMsg.signals:
         vals[msg.name] = 0
 
-    pycanMsg = can.Message(\
+    vals[sigName] = sigVal
+
+    return can.Message(\
                 arbitration_id = cantoolsMsg.frame_id,\
                 data = cantoolsMsg.encode(vals),
                 is_extended_id=False)
 
-    n_to_tx = 100
-    n_tx = 0
 
-    while n_to_tx > n_tx:
-        bus.send(msg=msg)
 
-        print('sent %1.0f messages' % n_tx)
-        time.sleep(0.01)
-        n_tx += 1
+n_to_tx = 1e6
+n_tx = 1
 
-    bus.shutdown()
+while n_to_tx > n_tx:
+
+    rpmMsg = create_message(db, 'engine_general_status_1', 'eng_speed', (8000/216) * n_tx)
+    bus.send(msg=rpmMsg)
+
+    speedMsg = create_message(db, 'vehicle_speed_and_distance', 'vehicle_speed_avg_driven', 2 * n_tx)
+    bus.send(speedMsg)
+
+    coolMsg = create_message(db, 'engine_general_status_4', 'eng_coolant_temp', n_tx)
+    bus.send(coolMsg)
+
+    print('sent %1.0f messages' % n_tx)
+    time.sleep(0.1)
+    n_tx += 1
+
+bus.shutdown()
