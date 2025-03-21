970614b480d02f9a59014e985c53990d193cf691
Victor Zaccardo <victorzaccardo@gmail.com>
2025-03-21 17:04:01 -0400

    Updated tx_can testing script

--- src/pyCANdash/examples/tx_can.py
+++ src/pyCANdash/examples/tx_can.py
@@ -34,22 +34,48 @@ def create_message(db, msgName, sigName, sigVal):
 
 
 
-n_to_tx = 1e6
-n_tx = 1
+n_to_tx = 215
+n_tx = 0
 
-while n_to_tx > n_tx:
+if 0: 
+    while n_to_tx + 1 > n_tx:
+        rpmMsg = create_message(db, 'engine_general_status_1', 'eng_speed', (8000/216) * n_tx)
+        bus.send(msg=rpmMsg)
 
-    rpmMsg = create_message(db, 'engine_general_status_1', 'eng_speed', (8000/216) * n_tx)
-    bus.send(msg=rpmMsg)
+        speedMsg = create_message(db, 'vehicle_speed_and_distance', 'vehicle_speed_avg_driven', 2 * n_tx)
+        bus.send(speedMsg)
 
-    speedMsg = create_message(db, 'vehicle_speed_and_distance', 'vehicle_speed_avg_driven', 2 * n_tx)
-    bus.send(speedMsg)
+        coolMsg = create_message(db, 'engine_general_status_4', 'eng_coolant_temp', n_tx)
+        bus.send(coolMsg)
 
-    coolMsg = create_message(db, 'engine_general_status_4', 'eng_coolant_temp', n_tx)
-    bus.send(coolMsg)
+        oilPmsg = create_message(db, 'engine_general_status_5', 'eng_oil_pressure', 1020 * n_tx/n_to_tx)
+        bus.send(oilPmsg)
 
-    print('sent %1.0f messages' % n_tx)
-    time.sleep(0.1)
-    n_tx += 1
+        oilPmsg = create_message(db, 'engine_general_status_5', 'check_engine_ind_on', n_tx % 2)
+        bus.send(oilPmsg)                                                         
+
+        print('sent %1.0f messages' % n_tx)
+        time.sleep(0.05)
+        n_tx += 1
+
+else:
+        rpmMsg = create_message(db, 'engine_general_status_1', 'eng_speed', 8000)
+        bus.send(msg=rpmMsg)
+        time.sleep(0.10)
+
+        speedMsg = create_message(db, 'vehicle_speed_and_distance', 'vehicle_speed_avg_driven', 15/0.6213712)
+        bus.send(speedMsg)
+        time.sleep(0.10)
+
+        coolMsg = create_message(db, 'engine_general_status_4', 'eng_coolant_temp', 100)
+        bus.send(coolMsg)
+        time.sleep(0.10)
+
+        oilPmsg = create_message(db, 'engine_general_status_5', 'eng_oil_pressure', 50*6.894)
+        bus.send(oilPmsg)
+        time.sleep(0.10)
+
+        # CELmsg = create_message(db, 'engine_general_status_5', 'check_engine_ind_on', 1)
+        # bus.send(CELmsg)    
 
 bus.shutdown()
