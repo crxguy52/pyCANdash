34b37a4f93fdfb2cd72630fa602998dda4ecd24c
Victor Zaccardo <victorzaccardo@gmail.com>
2025-08-11 13:43:38 -0400

    Update gui_config.py

--- config_files/gui_config.py
+++ config_files/gui_config.py
@@ -4,11 +4,12 @@
     "DATA_DIR"  : ["..", "data"],             # Where to save the data, path relative to app _internal folder. ["Data"] saves it to _internal\Data
     "DBC_DIR"   : ["..", "dbc"],              # Where CAN database files live, path relative to app _internal folder. ["config_files"] looks in _internal\config_files
     "LOGGER_CFG"  : "config_default",    # Logger config to use
-    "WINDOW_SIZE_MIN_WIDTH": 1024,      # Main window min width
-    "WINDOW_SIZE_MIN_HEIGHT": 600,      # Main window min height
+    "WINDOW_SIZE_MIN_WIDTH": 800,      # Main window min width
+    "WINDOW_SIZE_MIN_HEIGHT": 500,      # Main window min height
     "STATUS_ROWS": 4,                   # Number of scope status rows
     "STATUS_COLS": 4,                   # Number of scope status columns  
 }
 
 # fmt: on
+
 
