8ca318c358624b6dda6cda053924e9d94c13bdfe
Victor Zaccardo <victorzaccardo@gmail.com>
2025-10-30 10:41:59 -0400

    Update config_default.py

--- config_files/config_default.py
+++ config_files/config_default.py
@@ -183,7 +183,7 @@
                 # Origin is in the top left, so 00 is the top left gauge. Center gague is col 1, right gauges are col 2
                 'sideGauge00':{'signal':'eng_oil_temperature',   'lims':(60, 60, 75, 125, 135, 140),   'label':'Oil\n', 'convert2ips':False, 'nMainDivs':8,},
                 'sideGauge10':{'signal':'eng_oil_pressure',      'lims':(0, 10, 15, 120, 130, 140),      'label':'Oil\n', 'convert2ips':True, 'nMainDivs':14,},                         
-                'sideGauge02':{'signal':'eng_coolant_temp',      'lims':(60, 60, 75, 110, 115, 140),   'label':'Water\n', 'convert2ips':False, 'nMainDivs':8,},
+                'sideGauge02':{'signal':'eng_coolant_temp',      'lims':(60, 60, 75, 115, 120, 140),   'label':'Water\n', 'convert2ips':False, 'nMainDivs':8,},
                 'sideGauge12':{'signal':'trans_oil_temp',        'lims':(20, 20, 15, 100, 110, 120),   'label':'Trans\n', 'convert2ips':False, 'nMainDivs':10,},
                 'centerGauge':{'signal':'eng_speed',             'lims':(0, 0, 0, 6.8, 7.2, 8), 'label':'', 'convert2ips':True,        # if convert2ips is true, converts to kRPM
                                 'speedSignal':'vehicle_speed_avg_driven', 'dynamicRedline':True, 'speedUnit':'MPH', 'speedGain':0.621371},  # MPH per kph     
@@ -208,6 +208,7 @@
         },                
     },
 }
+
 
 
 
