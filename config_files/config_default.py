b0fcf559c3d4d61e3f63190a73788fb09e8b880c
Victor Zaccardo <victorzaccardo@gmail.com>
2026-08-16 14:37:12 -0400

    Update config_default.py

--- config_files/config_default.py
+++ config_files/config_default.py
@@ -211,7 +211,7 @@
                 'sideGauge00':{'signal':'eng_oil_temperature',   'lims':(60, 60, 75, 135, 145, 160),   'label':'Oil\n', 'convert2ips':False, 'nMainDivs':8,},
                 'sideGauge10':{'signal':'eng_oil_pressure',      'lims':(0, 10, 15, 120, 130, 140),      'label':'Oil\n', 'convert2ips':True, 'nMainDivs':14,},                         
                 'sideGauge02':{'signal':'eng_coolant_temp',      'lims':(60, 60, 75, 115, 120, 140),   'label':'Water\n', 'convert2ips':False, 'nMainDivs':8,},
-                'sideGauge12':{'signal':'trans_oil_temp',        'lims':(50, 50, 50, 130, 140, 150),   'label':'Trans\n', 'convert2ips':False, 'nMainDivs':11,},
+                'sideGauge12':{'signal':'trans_oil_temp',        'lims':(50, 50, 50, 130, 140, 150),   'label':'Trans\n', 'convert2ips':False, 'nMainDivs':10,},
                 'centerGauge':{'signal':'eng_speed',             'lims':(0, 0, 0, 6.8, 7.2, 8), 'label':'', 'convert2ips':True,        # if convert2ips is true, converts to kRPM
                                 'speedSignal':'vehicle_speed_avg_driven', 'dynamicRedline':True, 'speedUnit':'MPH', 'speedGain':0.621371},  # MPH per kph     
                                 # speed stuff is a hack but also I don't feel like doing it a better way
