18042be2b0550c266555dc40862446d3168f30ba
Victor Zaccardo <victorzaccardo@gmail.com>
2026-09-23 21:23:29 -0400

    Update config_default.py

--- config_files/config_default.py
+++ config_files/config_default.py
@@ -144,7 +144,7 @@
                     8:{'sigName':None,                               'dispName':'',              'lims':(-1e3, -999, 	1, 		1500,	2e3, 2001), 'dispFmt':'%1.0f' , 'convert2ips':False,},
                     9:{'sigName':None,                               'dispName':'',              'lims':(-1e3, -999, 	1, 		1500,	2e3, 2001), 'dispFmt':'%1.0f' , 'convert2ips':False,},
                     10:{'sigName':None,                              'dispName':'',              'lims':(-1e3, -999, 	1, 		1500,	2e3, 2001), 'dispFmt':'%1.0f' , 'convert2ips':False,},
-                    11:{'sigName':None,                              'dispName':'',              'lims':(-1e3, -999, 	1, 		1500,	2e3, 2001), 'dispFmt':'%1.0f' , 'convert2ips':False,},   
+                    11:{'sigName':'clutch_pedal_actual_pos',         'dispName':'Clutch Pos',    'lims':(-1e3, -999, 	-999, 	999,	999, 2001), 'dispFmt':'%1.0f' , 'convert2ips':False,},   
                     12:{'sigName':'lambda',                          'dispName':'WB02 Lambda',   'lims':(-1e3, -999, 	-998,	1500,	2e3, 2001), 'dispFmt':'%1.2f' , 'convert2ips':False,},
                     13:{'sigName':'lsu_temp',                        'dispName':'WBO2 Temp',     'lims':(-1e3, -999, 	-998,	1500,	2e3, 2001), 'dispFmt':'%1.0f' , 'convert2ips':False,},
                     14:{'sigName':'spartan_stat',                    'dispName':'WBO2 Status',   'lims':(-1e3, -999, 	-998,	1500,	2e3, 2001), 'dispFmt':'%1.0f' , 'convert2ips':False,},
@@ -208,10 +208,10 @@
                 # high      = start of yellow area
                 # highhigh  = start of red area
                 # ulim  = upper display limit (for gagues)
-                'sideGauge00':{'signal':'eng_oil_temperature',   'lims':(60, 60, 75, 135, 145, 160),   'label':'Oil\n', 'convert2ips':False, 'nMainDivs':8,},
+                'sideGauge00':{'signal':'eng_oil_temperature',   'lims':(60, 60, 75, 135, 145, 160),   'label':'Oil\n', 'convert2ips':False, 'nMainDivs':9,},
                 'sideGauge10':{'signal':'eng_oil_pressure',      'lims':(0, 10, 15, 120, 130, 140),      'label':'Oil\n', 'convert2ips':True, 'nMainDivs':14,},                         
                 'sideGauge02':{'signal':'eng_coolant_temp',      'lims':(60, 60, 75, 115, 120, 140),   'label':'Water\n', 'convert2ips':False, 'nMainDivs':8,},
-                'sideGauge12':{'signal':'trans_oil_temp',        'lims':(50, 50, 50, 130, 140, 150),   'label':'Trans\n', 'convert2ips':False, 'nMainDivs':10,},
+                'sideGauge12':{'signal':'lambda ',               'lims':(0.8, 0.8, 0.8, 1.15, 1.5, 1.2),   'label':'Lambda\n', 'convert2ips':False, 'nMainDivs':10,},
                 'centerGauge':{'signal':'eng_speed',             'lims':(0, 0, 0, 6.8, 7.2, 8), 'label':'', 'convert2ips':True,        # if convert2ips is true, converts to kRPM
                                 'speedSignal':'vehicle_speed_avg_driven', 'dynamicRedline':True, 'speedUnit':'MPH', 'speedGain':0.621371},  # MPH per kph     
                                 # speed stuff is a hack but also I don't feel like doing it a better way
